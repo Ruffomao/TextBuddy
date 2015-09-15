63c0e01b2b6691bc66d9f334c84f5823c0614ba7
Ruffomao <jtjudo93@gmail.com>
2015-09-16 00:15:29 +0800

    Major restructure of CE1
    1) Cleaned up the 'import' lines
    2) Included global String variables to avoid 'magic strings'
    3) Included global variables for File, Scanner and the required Writer
    and Reader objects to fit new structure.
    3) Included "enum OPERATION TYPE" to fit the new structure. Commented
    out the lengthy 'if' and 'else if' statements in
    'executeCommands(File)'. To be removed once all functions have been
    implemented.
    4) Refactored the main function to exclude object instantiation
    5) Included a parseCommand() to separate commands from content in the
    user entered string
    6) Replaced the lengthy 'if' and 'else if' statements with switch
    statements. Currently only contains 'add' and 'invalid' methods.
    7) Improved print methods from using magic strings to global string
    variables
    8) Included methods to instantiate the respective Writer and Reader
    objects

--- src/TextBuddy.java
+++ src/TextBuddy.java
@@ -1,13 +1,5 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 /*
  * This class allows user to create text files (.txt) and manipulate the contents within them on the command line.
@@ -18,12 +10,141 @@ import java.util.Scanner;
 public class TextBuddy {
 
 	private static boolean IS_ACCEPTING_COMMAND = true;
+	private static Scanner sc = new Scanner(System.in);
+	private static File file;
+	private static FileWriter writer;
+	private static BufferedWriter bufferedWriter;
+	private static FileReader reader;
+	private static BufferedReader bufferedReader;
+	private static PrintWriter printWriter;
+	private static ArrayList<String> sentencesArray;
+	private static ArrayList<String> commandArray;
+	// private static ArrayList<String> searchArray;
+	
+	/*
+	 * MESSAGES
+	 */
+	private static final char MESSAGE_INVERTED_COMMAS = (char)34;
+	private static final String MESSAGE_WELCOME_1 = "Welcome to TextBuddy++. ";
+	private static final String MESSAGE_WELCOME_2 = " is ready for use";
+	private static final String MESSAGE_COMMAND_PROMPT = "command: ";
+	private static final String MESSAGE_INVALID = "Invalid command. Please enter a valid command";
+	private static final String MESSAGE_ADD_SUCCESS = " successfully added to ";
+	private static final String MESSAGE_DISPLAY_EMPTY = " is empty";
+	private static final String MESSAGE_CLEAR_SUCCESS = "All content cleared from ";
+	private static final String MESSAGE_DELETE_FAIL_1 = "Failed to delete line ";
+	private static final String MESSAGE_DELETE_FAIL_2 = " from ";
+	private static final String MESSAGE_DELETE_SUCCESS = " successfully deleted from ";
+	private static final String MESSAGE_SORT_SUCCESS = " has been sorted successfully";
+	
+	/*
+	 * OPERATIONS
+	 */
+	enum OPERATION_TYPE {
+		ADD, DISPLAY, DELETE, CLEAR, EXIT, INVALID, SORT, SEARCH
+	};
 
 	public static void main(String[] args) throws IOException {
-		String fileName = args[0];
-		File file = createFile(fileName);
-		executeCommands(file);
+		initiateProgram(args);
+		runProgram();
+	}
+	
+	private static void initiateProgram(String[] args) throws IOException {
+		file = new File(args[0]);
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+		printWelcome(args);
+	}
+	
+	private static void runProgram() throws IOException {
+		while (IS_ACCEPTING_COMMAND) {
+			printCommandPrompt();
+			executeCommand(parseCommand());
+		}
+	}
+	
+	private static ArrayList<String> parseCommand() {
+		String userCommand = sc.nextLine();
+		commandArray = new ArrayList<String>();
+		commandArray.add(getOperation(userCommand));
+		commandArray.add(getCommand(userCommand));
+		return commandArray;
+	}
+	
+	private static String getCommand(String userCommand) {
+		return userCommand.replace(getOperation(userCommand), "").trim();
+	}
 
+	private static String getOperation(String userCommand) {
+		return userCommand.trim().split("\\s++")[0];
+	}
+	
+	private static void executeCommand(ArrayList<String> command) throws IOException {
+		OPERATION_TYPE operationType = getOperationType(command.get(0));
+		String operationContent = command.get(1);
+
+		switch (operationType) {
+		case ADD:
+			operationAdd(operationContent);
+			return;
+		/*case DISPLAY:
+			operationDisplay();
+			return;
+		case CLEAR:
+			operationClear();
+			return;
+		case DELETE:
+			operationDelete(operationContent);
+			return;
+		case SORT:
+			operationSort();
+			return;
+		/*
+		 * case SEARCH: operationSearch(); return;
+		 */
+		case EXIT:
+			IS_ACCEPTING_COMMAND = false;
+			return;
+		default:
+			operationInvalid();
+			return;
+		}
+	}
+	
+	private static OPERATION_TYPE getOperationType(String userOperation) {
+		if (userOperation.equalsIgnoreCase("add")) {
+			return OPERATION_TYPE.ADD;
+		} else if (userOperation.equalsIgnoreCase("display")) {
+			return OPERATION_TYPE.DISPLAY;
+		} else if (userOperation.equalsIgnoreCase("delete")) {
+			return OPERATION_TYPE.DELETE;
+		} else if (userOperation.equalsIgnoreCase("clear")) {
+			return OPERATION_TYPE.CLEAR;
+		} else if (userOperation.equalsIgnoreCase("exit")) {
+			return OPERATION_TYPE.EXIT;
+		} else if (userOperation.equalsIgnoreCase("sort")) {
+			return OPERATION_TYPE.SORT;
+		} else if (userOperation.equalsIgnoreCase("search")) {
+			return OPERATION_TYPE.SEARCH;
+		} else {
+			return OPERATION_TYPE.INVALID;
+		}
+	}
+	
+	private static void operationInvalid() {
+		printCommandInvalid();
+	}
+	
+	private static void operationAdd(String operationContent) throws IOException {
+		writeToFile(operationContent);
+		printAddSuccess(operationContent);
+	}
+	
+	private static void writeToFile(String content) throws IOException {
+		PrintWriter printWriter = createWriter();
+		printWriter.println(content);
+		printWriter.close();
 	}
 
 	/*
@@ -35,7 +156,7 @@ public class TextBuddy {
 	 * 
 	 * @param file
 	 */
-	private static void executeCommands(File file) throws IOException, FileNotFoundException {
+	/*private static void executeCommands(File file) throws IOException, FileNotFoundException {
 		Scanner sc = new Scanner(System.in);
 		while (IS_ACCEPTING_COMMAND) {
 			String userCommand = askForUserCommand(sc);
@@ -83,7 +204,7 @@ public class TextBuddy {
 			}
 		}
 		sc.close();
-	}
+	}*/
 
 	/*
 	 * This method copies the contents in the given arrayList onto the file.
@@ -114,48 +235,6 @@ public class TextBuddy {
 			textArray.add(br.readLine());
 		}
 		return textArray;
-	}
-
-	// Prints a message when the user has input an invalid command
-	private static void printNonValidAction() {
-		System.out.println("please input a valid command: ADD, DISPLAY, DELETE, CLEAR");
-	}
-
-	/* Prints a message when a line of text has been successfully deleted
-	 * 
-	 * @param file: the target file
-	 * 		  textToBeDeleted: the string which is to be deleted
-	 */
-	private static void printDeleteAction(File file, String textToBeDeleted) {
-		System.out.println("deleted from " + file + ": " + (char) 34 + textToBeDeleted + (char) 34);
-	}
-
-	/* Prints a message when a line of text cannot be deleted
-	 * 
-	 * @param file: the target file
-	 * 		  lineToBeDeleted: the line number which the user wants to be deleted
-	 */
-	private static void printDeleteFailedAction(File file, int lineToBeDeleted) {
-		System.out.println("unable to delete line " + lineToBeDeleted + " from " + file);
-	}
-
-	/* Prints a message when all contents in a file has been successfully
-	 * deleted
-	 * 
-	 * @param file: the target file
-	 */
-	private static void printClearAction(File file) {
-		System.out.println("all content deleted from " + file);
-	}
-
-	/* Prints a message when a string has been successfully appended onto the 
-	 * file
-	 * 
-	 * @param file: the target file
-	 * 		  userInputString: the string which the user wants added to the file
-	 */
-	private static void printAddAction(File file, String userInputString) {
-		System.out.println("added to " + file + ": " + (char) 34 + userInputString.trim() + (char) 34);
 	}
 
 	// This method scans for the user's input and returns it as a string
@@ -193,13 +272,6 @@ public class TextBuddy {
 	private static void printLine(BufferedReader br) throws IOException {
 		String lineToPrint = br.readLine();
 		System.out.print(lineToPrint);
-	}
-
-	// This method prepares a file to be read
-	private static BufferedReader readFile(File file) throws FileNotFoundException {
-		FileReader fr = new FileReader(file);
-		BufferedReader br = new BufferedReader(fr);
-		return br;
 	}
 
 	/* This method appends a string to the end of a file beginning on a new line
@@ -242,21 +314,6 @@ public class TextBuddy {
 		return textToBeDeleted;
 	}
 
-	/* This method creates a new file if a file with a similar name has not been created yet
-	 * 
-	 * @param fileName: the name of the file the user wants to create
-	 * 
-	 * @return returns a newly created file or an existing file if the name specified already exists
-	 */
-	private static File createFile(String fileName) throws IOException {
-		File file = new File(fileName);
-		if (!file.exists()) {
-			file.createNewFile();
-		}
-		System.out.println("Welcome to Textbuddy. " + file + " is ready for use");
-		return file;
-	}
-
 	/* This method counts the number of lines of text in a file and returns it as an integer
 	 * 
 	 * @param file: the target file
@@ -272,5 +329,37 @@ public class TextBuddy {
 		br.close();
 		return numberOfLines;
 	}
+	
+	private static BufferedReader createReader() throws FileNotFoundException {
+		reader = new FileReader(file);
+		bufferedReader = new BufferedReader(reader);
+		return bufferedReader;
+	}
 
+	private static PrintWriter createWriter() throws IOException {
+		writer = new FileWriter(file, true);
+		bufferedWriter = new BufferedWriter(writer);
+		printWriter = new PrintWriter(bufferedWriter);
+		return printWriter;
+	}
+	
+	/*
+	 * PRINT METHODS
+	 */
+	
+	private static void printWelcome(String[] args) {
+		System.out.println(MESSAGE_WELCOME_1 + args[0] + MESSAGE_WELCOME_2);
+	}
+	
+	private static void printCommandPrompt() {
+		System.out.print(MESSAGE_COMMAND_PROMPT);
+	}
+	
+	private static void printCommandInvalid() {
+		System.out.println(MESSAGE_INVALID);
+	}
+	
+	private static void printAddSuccess(String content) {
+		System.out.println(MESSAGE_INVERTED_COMMAS + content + MESSAGE_INVERTED_COMMAS + MESSAGE_ADD_SUCCESS + file);
+	}
 }
