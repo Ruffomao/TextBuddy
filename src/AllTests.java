56dda236711b93b32f99cc38ab1e292c1ae317a4
Ruffomao <jtjudo93@gmail.com>
2015-09-17 00:11:00 +0800

    Included Test Suite
    Created a test suite for: GetCommandTest.class, GetOperationTest.class
    and GetSpecificLineTest.class

---
+++ src/AllTests.java
@@ -0,0 +1,9 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ GetCommandTest.class, GetOperationTest.class, GetSpecificLineTest.class })
+public class AllTests {
+
+}
