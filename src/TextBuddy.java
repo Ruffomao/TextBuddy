d3d46ec48b5ad5e6565bd84260c257d594955048
Ruffomao <jtjudo93@gmail.com>
2015-09-16 15:20:26 +0800

    Commenting
    1) Commented on relevant methods
    2) Formatted the code slightly

--- src/TextBuddy.java
+++ src/TextBuddy.java
@@ -17,14 +17,11 @@ public class TextBuddy {
 	private static FileReader reader;
 	private static BufferedReader bufferedReader;
 	private static PrintWriter printWriter;
-	// private static ArrayList<String> sentencesArray;
-	// private static ArrayList<String> commandArray;
-	// private static ArrayList<String> searchArray;
-	
+
 	/*
 	 * MESSAGES
 	 */
-	private static final char MESSAGE_INVERTED_COMMAS = (char)34;
+	private static final char MESSAGE_INVERTED_COMMAS = (char) 34;
 	private static final String MESSAGE_WELCOME_1 = "Welcome to TextBuddy++. ";
 	private static final String MESSAGE_WELCOME_2 = " is ready for use";
 	private static final String MESSAGE_COMMAND_PROMPT = "command: ";
@@ -36,7 +33,7 @@ public class TextBuddy {
 	private static final String MESSAGE_DELETE_FAIL_2 = " from ";
 	private static final String MESSAGE_DELETE_SUCCESS = " successfully deleted from ";
 	private static final String MESSAGE_SORT_SUCCESS = " has been sorted successfully";
-	
+
 	/*
 	 * OPERATIONS
 	 */
@@ -48,7 +45,7 @@ public class TextBuddy {
 		initiateProgram(args);
 		runProgram();
 	}
-	
+
 	private static void initiateProgram(String[] args) throws IOException {
 		file = new File(args[0]);
 		if (!file.exists()) {
@@ -56,14 +53,18 @@ public class TextBuddy {
 		}
 		printWelcome(args);
 	}
-	
+
 	private static void runProgram() throws IOException {
 		while (IS_ACCEPTING_COMMAND) {
 			printCommandPrompt();
 			executeCommand(parseCommand());
 		}
 	}
-	
+
+	/*
+	 * Parses the user entered string and returns an ArrayList<String>
+	 * containing the command and the relevant contents
+	 */
 	private static ArrayList<String> parseCommand() {
 		String userCommand = sc.nextLine();
 		ArrayList<String> commandArray = new ArrayList<String>();
@@ -71,7 +72,7 @@ public class TextBuddy {
 		commandArray.add(getCommand(userCommand));
 		return commandArray;
 	}
-	
+
 	private static String getCommand(String userCommand) {
 		return userCommand.replace(getOperation(userCommand), "").trim();
 	}
@@ -79,7 +80,14 @@ public class TextBuddy {
 	private static String getOperation(String userCommand) {
 		return userCommand.trim().split("\\s++")[0];
 	}
-	
+
+	/*
+	 * Executes the relevant methods depending on the command entered by the
+	 * user
+	 * 
+	 * @param command: An ArrayList containing the command and its relevant
+	 * content
+	 */
 	private static void executeCommand(ArrayList<String> command) throws IOException {
 		OPERATION_TYPE operationType = getOperationType(command.get(0));
 		String operationContent = command.get(1);
@@ -97,11 +105,9 @@ public class TextBuddy {
 		case DELETE:
 			operationDelete(operationContent);
 			return;
-		/*case SORT:
-			operationSort();
-			return;
 		/*
-		 * case SEARCH: operationSearch(); return;
+		 * case SORT: operationSort(); return; /* case SEARCH:
+		 * operationSearch(); return;
 		 */
 		case EXIT:
 			IS_ACCEPTING_COMMAND = false;
@@ -111,7 +117,10 @@ public class TextBuddy {
 			return;
 		}
 	}
-	
+
+	/*
+	 * Casts a string type into an OPERATION_TYPE
+	 */
 	private static OPERATION_TYPE getOperationType(String userOperation) {
 		if (userOperation.equalsIgnoreCase("add")) {
 			return OPERATION_TYPE.ADD;
@@ -131,16 +140,16 @@ public class TextBuddy {
 			return OPERATION_TYPE.INVALID;
 		}
 	}
-	
+
 	private static void operationInvalid() {
 		printCommandInvalid();
 	}
-	
+
 	private static void operationAdd(String operationContent) throws IOException {
 		writeToFile(operationContent);
 		printAddSuccess(operationContent);
 	}
-	
+
 	private static void operationDisplay() throws IOException {
 		int numOfLines = countNumOfLines();
 		if (numOfLines > 0) {
@@ -150,12 +159,12 @@ public class TextBuddy {
 			printDisplayEmpty();
 		}
 	}
-	
+
 	static void operationClear() throws IOException {
 		clearAllContent();
 		printClearSuccess();
 	}
-	
+
 	private static void operationDelete(String content) throws IOException {
 		int lineToDelete = new Integer(content);
 		int numOfLines = countNumOfLines();
@@ -166,13 +175,27 @@ public class TextBuddy {
 			printDeleteSuccess(textToBeDeleted);
 		}
 	}
-	
+
+	/*
+	 * Writes a string onto the file
+	 * 
+	 * @param content: the string which is to be written onto the file
+	 */
 	private static void writeToFile(String content) throws IOException {
 		createWriter();
 		printWriter.println(content);
 		printWriter.close();
 	}
-	
+
+	/*
+	 * Deletes a single line of text on the file
+	 * 
+	 * @param lineToDelete: the line which is to be deleted
+	 * 
+	 * @param numOfLines: the number of lines on the file
+	 * 
+	 * @return the string which has been deleted
+	 */
 	private static String deleteLine(int lineToDelete, int numOfLines) throws IOException {
 		ArrayList<String> sentencesArray = new ArrayList<String>();
 		sentencesArray = contentToArray(numOfLines);
@@ -187,8 +210,8 @@ public class TextBuddy {
 	 * This method copies the contents in the given arrayList onto the file.
 	 * Contents are copied in the order they are stored on the arrayList.
 	 * 
-	 * @param file: the target file 
-	 * 		  textArray: the arrayList object used in storing the contents of the file
+	 * @param file: the target file textArray: the arrayList object used in
+	 * storing the contents of the file
 	 * 
 	 */
 	private static void arrayToFile(ArrayList<String> textArray) throws IOException {
@@ -206,18 +229,25 @@ public class TextBuddy {
 		printWriter.close();
 	}
 
-	/* This method retrieves the string on a specific line in the file
+	/*
+	 * This method retrieves the string on a specific line in the file
 	 * 
-	 * @param file: the target file
-	 * 		  lineToBeDeleted: the line of text which the user wants deleted from the file
+	 * @param textArray: the ArrayList which contains the line of text to be
+	 * deleted
 	 * 
-	 * @return the string in that line
+	 * @param lineIndex: the line of text which the user wants deleted from the
+	 * file
+	 * 
+	 * @return the line of text
 	 */
-	private static String getSpecificLine(ArrayList<String> textArray, int lineIndex) throws FileNotFoundException, IOException {
+	private static String getSpecificLine(ArrayList<String> textArray, int lineIndex)
+			throws FileNotFoundException, IOException {
 		return textArray.get(lineIndex - 1);
 	}
 
-	/* This method counts the number of lines of text in a file and returns it as an integer
+	/*
+	 * This method counts the number of lines of text in a file and returns it
+	 * as an integer
 	 * 
 	 * @param file: the target file
 	 * 
@@ -233,6 +263,14 @@ public class TextBuddy {
 		return numberOfLines;
 	}
 	
+	/*
+	 * Copies the content on the file onto an ArrayList<String>. Each line of text will
+	 * occupy an index
+	 * 
+	 * @param numOfLines: the number of lines in the file
+	 * 
+	 * @return an ArrayList<String> with the contents of the file
+	 */
 	private static ArrayList<String> contentToArray(int numOfLines) throws IOException {
 		createReader();
 		ArrayList<String> textArray = new ArrayList<String>();
@@ -243,53 +281,59 @@ public class TextBuddy {
 		return textArray;
 	}
 	
+	/*
+	 * Instantiates the FileReader and BufferedReader objects
+	 */
 	private static void createReader() throws FileNotFoundException {
 		reader = new FileReader(file);
 		bufferedReader = new BufferedReader(reader);
 	}
-
+	
+	/*
+	 * Instantiates the FileWriter, BufferedWriter and PrintWriter objects
+	 */
 	private static void createWriter() throws IOException {
 		writer = new FileWriter(file, true);
 		bufferedWriter = new BufferedWriter(writer);
 		printWriter = new PrintWriter(bufferedWriter);
 	}
-	
+
 	/*
 	 * PRINT METHODS
 	 */
-	
 	private static void printWelcome(String[] args) {
 		System.out.println(MESSAGE_WELCOME_1 + args[0] + MESSAGE_WELCOME_2);
 	}
-	
+
 	private static void printCommandPrompt() {
 		System.out.print(MESSAGE_COMMAND_PROMPT);
 	}
-	
+
 	private static void printCommandInvalid() {
 		System.out.println(MESSAGE_INVALID);
 	}
-	
+
 	private static void printAddSuccess(String content) {
 		System.out.println(MESSAGE_INVERTED_COMMAS + content + MESSAGE_INVERTED_COMMAS + MESSAGE_ADD_SUCCESS + file);
 	}
-	
+
 	private static void printClearSuccess() {
 		System.out.println(MESSAGE_CLEAR_SUCCESS + file);
 	}
-	
+
 	private static void printDeleteFail(Integer lineToDelete) {
 		System.out.println(MESSAGE_DELETE_FAIL_1 + lineToDelete + MESSAGE_DELETE_FAIL_2 + file);
 	}
 
 	private static void printDeleteSuccess(String textToBeDeleted) {
-		System.out.println(MESSAGE_INVERTED_COMMAS + textToBeDeleted + MESSAGE_INVERTED_COMMAS + MESSAGE_DELETE_SUCCESS + file);
+		System.out.println(
+				MESSAGE_INVERTED_COMMAS + textToBeDeleted + MESSAGE_INVERTED_COMMAS + MESSAGE_DELETE_SUCCESS + file);
 	}
-	
+
 	private static void printDisplayEmpty() {
 		System.out.println(file + MESSAGE_DISPLAY_EMPTY);
 	}
-	
+
 	private static void printOrderedList(ArrayList<String> stringArray, int numOfLines) throws IOException {
 		stringArray = contentToArray(numOfLines);
 		int index = 1;
