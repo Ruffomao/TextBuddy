552cd03fe0da4b0f2c34463237794d3436af2423
Ruffomao <jtjudo93@gmail.com>
2015-09-18 19:27:36 +0800

    Written more tests under ExecuteSearchTest()
    Included 2 more tests for ExecuteSearchTest()

--- src/ExecuteSearchTest.java
+++ src/ExecuteSearchTest.java
@@ -1,6 +1,5 @@
 import static org.junit.Assert.*;
 
-import java.io.*;
 import java.util.ArrayList;
 
 import org.junit.Test;
@@ -25,7 +24,34 @@ public class ExecuteSearchTest {
 	
 	@Test
 	public void testSearchForWord2() {
+		ArrayList<String> testArray = new ArrayList<String>();
+		testArray.add("EFI");
+		testArray.add("ABI");
+		testArray.add("CDI");
 		
+		ArrayList<String> resultArray = new ArrayList<String>();
+		resultArray = TextBuddy.searchForWord(testArray, "I");
+		
+		Object[] result = new String[resultArray.size()];
+		result = resultArray.toArray();
+		String[] expected = {"ABI", "CDI", "EFI"};
+		assertArrayEquals(expected, result);
+	}
+	
+	@Test
+	public void testSearchForWord3() {
+		ArrayList<String> testArray = new ArrayList<String>();
+		testArray.add("ABC");
+		testArray.add("ABC");
+		testArray.add("ABC");
+		
+		ArrayList<String> resultArray = new ArrayList<String>();
+		resultArray = TextBuddy.searchForWord(testArray, "A");
+		
+		Object[] result = new String[resultArray.size()];
+		result = resultArray.toArray();
+		String[] expected = {"ABC"};
+		assertArrayEquals(expected, result);
 	}
 
 }
