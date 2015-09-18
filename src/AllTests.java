896594025c28b06e7689d7170e9d6c663a3880f9
Ruffomao <jtjudo93@gmail.com>
2015-09-18 19:28:12 +0800

    Included ExecuteSearchTest in test suite

--- src/AllTests.java
+++ src/AllTests.java
@@ -3,7 +3,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ GetCommandTest.class, GetOperationTest.class, GetSpecificLineTest.class, ExecuteSortTest.class })
+@SuiteClasses({ GetCommandTest.class, GetOperationTest.class, GetSpecificLineTest.class, ExecuteSortTest.class, ExecuteSearchTest.class })
 public class AllTests {
 
 }
